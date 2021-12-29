5ce8f4729cc0311e36a4b3300c769f9e60232f0e
Mike Pratt <mike@msjp.me>
2021-12-29 19:30:36 +0000

    Fixed button styling in index.

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,9 +24,13 @@ const Home: NextPage = () => {
             <h1 className="text-4xl font-semibold text-center">Your Games, Your Time</h1>
             <p className="text-2xl text-center">Keep track of your library, what you&apos;re currently playing,
             where you&apos;re up to, and your backlog.</p>
-            <div className="flex justify-center">
-              <Button onClick={() => goToPage('/about')}>Learn more</Button>
-              <Button onClick={() => goToPage('/sign-up')}>Sign Up</Button>
+            <div className="flex justify-center pt-2">
+              <div>
+                <Button onClick={() => goToPage('/about')}>Learn more</Button>
+              </div>
+              <div className="ml-4">
+                <Button onClick={() => goToPage('/sign-up')}>Sign Up</Button>
+              </div>
             </div>
           </div>
         </div>
