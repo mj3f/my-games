7213b4ba736c2a9d7c2dc7e598ff3b776f113b33
Mike Pratt <mike@msjp.me>
2021-12-30 16:05:46 +0000

    Started work on showing games library for user. Added twitch developer secrets.json file to gitignore.

--- components/Home/UserHome.tsx
+++ components/Home/UserHome.tsx
@@ -5,8 +5,16 @@ import AppContext from "../../context/AppContext";
 const UserHome: React.FC = () => {
     const [appState, _] = useContext(AppContext);
     return (
-        <div className="px-2">
-            <p className="text-3xl font-semibold">Welcome {appState.authState?.currentUser?.username}</p>
+        <div className="px-2 flex flex-col justify-start">
+            <p className="text-4xl font-semibold">{appState.authState?.currentUser?.username}&apos;s Library</p>
+            <div className="pt-4">
+                <p className="text-3xl font-semibold">In Progress</p>
+                <div id="collection" className="flex flex-row justify-start">
+                    <p>test 1</p>
+                    <p>test 2</p>
+                    <p>test 3</p>
+                </div>
+            </div>
         </div>
     );
 };
