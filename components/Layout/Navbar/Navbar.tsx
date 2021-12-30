849d9c46eac4bd1e12b532d70e80cc92d188f416
Mike Pratt <mike@msjp.me>
2021-12-30 14:01:18 +0000

    Navbar now shows different links if the user is logged in.

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { useContext, useEffect, useState } from 'react';
+import AppContext from '../../../context/AppContext';
 
 interface NavbarLinkProps {
     href: string;
@@ -12,13 +14,35 @@ const NavbarLink: React.FC<NavbarLinkProps> = ({ href, children }) => (
 );
 
 const Navbar: React.FC = () => {
+    const [appState, _] = useContext(AppContext);
+    const [showLoggedInNavLinks, setShowLoggedInNavLinks] = useState(false);
+
+
+    useEffect(() => {
+        setShowLoggedInNavLinks(appState.authState?.isAuthenticated);
+    }, [appState]);
+
+    const defaultNavLinks = (
+        <>
+            <NavbarLink href='/'>Home</NavbarLink>
+            <NavbarLink href='/about'>About</NavbarLink>
+            <NavbarLink href='/sign-in'>Sign In</NavbarLink>
+        </>
+    );
+
+    const loggedInNavLinks = (
+        <>
+            <NavbarLink href='/'>Library</NavbarLink>
+            <NavbarLink href='/sign-out'>Sign Out</NavbarLink>
+        </>
+    );
+
+
     return (
         <div className="flex w-full h-16 justify-between items-center bg-gray-900">
             <h1 className="text-3xl font-semibold pl-2 text-green-500">myGames</h1>
             <div id="links-container" className="h-full px-2 flex items-center justify-end">
-                <NavbarLink href='/'>Home</NavbarLink>
-                <NavbarLink href='/about'>About</NavbarLink>
-                <NavbarLink href='/sign-in'>Sign In</NavbarLink>
+                {showLoggedInNavLinks ? loggedInNavLinks : defaultNavLinks}
             </div>
         </div>
     );
