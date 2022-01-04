0acc01e028a06a533bc393d4d15ed54191dabdd5
Mike Pratt <mike@msjp.me>
2022-01-04 11:48:31 +0000

    Added models to frontend. Removed unused files.

--- src/MyGames/MyGames.Frontend/models/user/user.model.ts
+++ src/MyGames/MyGames.Frontend/models/user/user.model.ts
@@ -1,4 +1,8 @@
+import { Game } from "../game/game.model";
 
 export class User {
-    constructor(public username: string) {}
+    
+    constructor(
+        public username: string,
+        public games: Game[]) {}
 }
